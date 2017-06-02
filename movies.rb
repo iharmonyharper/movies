6080fc35369d34d1e85347e70828ceb98eeb2146
NnNn <iamharmonyharper@gmail.com>
2017-06-02 07:18:37 +0300

    Add filter

---
+++ movies.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+HEADERS = [:link, :name, :year, :country, :premier, :genre, :duration, :rating, :director, :stars]
+
+filename, keyword = ARGV if ARGV.count > 1
+keyword, filename = ARGV if ARGV.count < 2
+
+file = filename || 'movies.txt'
+raise "File '#{file}' is not found" unless File.exist?(file)
+
+movies = CSV.read(file, :col_sep => '|').map {|movie| Hash[HEADERS.zip(movie)]}
+
+
+def filter(movies, keyword)
+  movies.select {|movie| movie[:name][/#{keyword}/]}.map {|movie| "#{movie[:name]}, #{rating(movie[:rating])}"}
+end
+
+def rating(rating_float)
+  '*' * ((rating_float.to_f - 8.0).round(1) * 10)
+end
+
+puts filter(movies, keyword)
