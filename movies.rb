97e8cc7c2634fd54340e9dfb9308bef8a2a9cbd7
NnNn <iamharmonyharper@gmail.com>
2017-06-03 16:26:52 +0300

    Task 4

--- movies.rb
+++ movies.rb
@@ -1,4 +1,6 @@
 require 'csv'
+require 'ostruct'
+require 'date'
 
 HEADERS = [:link, :name, :year, :country, :premier, :genre, :duration, :rating, :director, :stars]
 
@@ -8,10 +10,10 @@ keyword, filename = ARGV if ARGV.count < 2
 file = filename || 'movies.txt'
 raise "File '#{file}' is not found" unless File.exist?(file)
 
-movies = CSV.read(file, :col_sep => '|').map {|movie| HEADERS.zip(movie).to_h}
+movies = CSV.read(file, :col_sep => '|').map {|movie| OpenStruct.new(HEADERS.zip(movie).to_h)}
 
 def filter(movies, keyword)
-  movies.select {|movie| movie[:name][/#{keyword}/]}
+  movies.select {|movie| movie.name[/#{keyword}/]}
 end
 
 def rating(rating_float)
@@ -20,10 +22,17 @@ def rating(rating_float)
 end
 
 def pretty_print(movies)
-  puts movies.map {|m| "#{rating m[:rating]}; #{m[:name]} (#{m[:premier]}; #{m[:genre]}) - #{m[:duration]}"}
+  puts movies.map {|m| "#{rating m.rating}; #{m.name} (#{m.premier}; #{m.genre}) - #{m.duration}"}
 end
 
-pretty_print(movies.sort_by {|m| m[:duration].to_i}.last(5))
-pretty_print(movies.select {|m| m[:genre].include? 'Comedy'}.sort_by {|m| m[:duration].to_i})
-puts (movies.map {|m| m[:director]}.sort_by {|name| name.split(' ').last})
-puts movies.count {|m| !m[:country].include? 'USA'}
+pretty_print(movies.sort_by {|m| m.duration.to_i}.last(5))
+pretty_print(movies.select {|m| m.genre.include? 'Comedy'}.sort_by {|m| m.duration.to_i})
+puts (movies.map(&:director).sort_by {|name| name.split(' ').last})
+puts movies.count {|m| !m.country.include? 'USA'}
+
+puts statistics = movies.group_by {|m|
+  Date.strptime(m.premier, '%Y-%m').mon rescue 'Invalid date'}
+                 .reject {|k, v| k == 'Invalid date'}
+                 .sort_by {|k, v| k}
+                 .map {|k, v| [Date::MONTHNAMES[k], v.count]
+}.to_h
