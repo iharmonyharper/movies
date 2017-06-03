751a600f645cbcbe7fa950d6b3857311b68501a7
NnNn <iamharmonyharper@gmail.com>
2017-06-03 17:53:21 +0300

    Task 4* -  %i{}, #uniq -  :headers option -  group_by(&:itself)

--- movies.rb
+++ movies.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'ostruct'
 require 'date'
 
-HEADERS = [:link, :name, :year, :country, :premier, :genre, :duration, :rating, :director, :stars]
+HEADERS = %i{link name year country premier genre duration rating director stars}
 
 filename, keyword = ARGV if ARGV.count > 1
 keyword, filename = ARGV if ARGV.count < 2
@@ -10,7 +10,9 @@ keyword, filename = ARGV if ARGV.count < 2
 file = filename || 'movies.txt'
 raise "File '#{file}' is not found" unless File.exist?(file)
 
-movies = CSV.read(file, :col_sep => '|').map {|movie| OpenStruct.new(HEADERS.zip(movie).to_h)}
+movies = CSV.read(file, headers: HEADERS, col_sep: '|').map {|movie|
+  OpenStruct.new(movie.to_h)
+}
 
 def filter(movies, keyword)
   movies.select {|movie| movie.name[/#{keyword}/]}
@@ -25,14 +27,13 @@ def pretty_print(movies)
   puts movies.map {|m| "#{rating m.rating}; #{m.name} (#{m.premier}; #{m.genre}) - #{m.duration}"}
 end
 
-pretty_print(movies.sort_by {|m| m.duration.to_i}.last(5))
-pretty_print(movies.select {|m| m.genre.include? 'Comedy'}.sort_by {|m| m.duration.to_i})
-puts (movies.map(&:director).sort_by {|name| name.split(' ').last})
+pretty_print(movies.sort_by {|m| m.duration.to_i}.last(5).uniq)
+pretty_print(movies.select {|m| m.genre.include? 'Comedy'}.sort_by {|m| m.duration.to_i}.uniq)
+puts (movies.map(&:director).sort_by {|name| name.split(' ').last}).uniq
 puts movies.count {|m| !m.country.include? 'USA'}
 
-puts statistics = movies.group_by {|m|
-  Date.strptime(m.premier, '%Y-%m').mon rescue 'Invalid date'}
-                 .reject {|k, v| k == 'Invalid date'}
-                 .sort_by {|k, v| k}
-                 .map {|k, v| [Date::MONTHNAMES[k], v.count]
-}.to_h
+puts statistics = movies.map {|m| Date.strptime(m.premier, '%Y-%m').mon rescue nil}
+                      .compact
+                      .group_by(&:itself)
+                      .sort_by(&:first)
+                      .map {|k, v| [Date::MONTHNAMES[k], v.count]}.to_h
